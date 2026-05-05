b6e4fc0955909e0514032a903fdc0e3c05877cca
Mamisoa Fitiavana Christ Offert Hendriniaina <mahendri@student.42antananarivo.mg>
2026-05-05 16:08:58 +0300

    ex2: Done

--- Python 08/ex2/oracle.py
+++ Python 08/ex2/oracle.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+
+import os
+from dotenv import load_dotenv
+
+
+def missings(envs: list[str]) -> list[str]:
+    misses: list[str] = []
+    for env in envs:
+        res: str | None = os.environ.get(env)
+        if not res:
+            misses += [env]
+    return misses
+
+
+def check_env(envs: list[str], misses: list[str]) -> bool:
+    res = {
+        "MATRIX_MODE": "Mode", "DATABASE_URL": "Database",
+        "API_KEY": "API Access",
+        "LOG_LEVEL": "Log Level",
+                       "ZION_ENDPOINT": "Zion Network",
+    }
+    print("Configuration loaded:")
+    for env in envs:
+        label: str | None = os.environ.get(env)
+        if env == "API_KEY":
+            label = "Authenticated" if label else "Not Authenticated"
+        if env == "DATABASE_URL":
+            label = ("Connected to local instance"
+                     if label == "localhost" or (not label)
+                     else label)
+        is_missing = f"{" [MISSING]" if env in misses else ""}"
+        print(f"{res[env]}: {label}{is_missing}")
+    if len(misses):
+        return False
+    return True
+
+
+def oracle() -> None:
+    print("ORACLE STATUS: Reading the Matrix...\n")
+    _ = load_dotenv()
+
+    envs: list[str] = ["MATRIX_MODE", "DATABASE_URL", "API_KEY", "LOG_LEVEL",
+                       "ZION_ENDPOINT",]
+    misses = missings(envs)
+
+    if not check_env(envs, misses):
+        return
+
+    print("\nEnvironment security check:")
+    print("[OK] No hardcoded secrets detected")
+    print("[OK] .env file properly configured")
+    print("[OK] Production overrides available")
+    print("\nThe Oracle sees all configurations.")
+
+
+if __name__ == "__main__":
+    oracle()
